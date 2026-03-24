fe65f391afe7b3e853949a0a73d4c20c5275fd4f
Jose-RCastle <143913314+Jose-RCastle@users.noreply.github.com>
2026-03-23 19:58:04 -0600

    Redux Toolkit | Se instalaron dependencias base
    Dependencias instaladas con npm install && npx expo install --fix

--- MoneyLog/App.tsx
+++ MoneyLog/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+//import { Provider } from 'react-redux';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
+//import { store } from './src/store/store';
 
 // ============================================================
 // PUNTO DE ENTRADA — MoneyLog
@@ -12,14 +14,15 @@ import AppNavigator from './src/navigation/AppNavigator';
 // con <Provider store={store}>.
 // ============================================================
 
+
 export default function App() {
   return (
     <SafeAreaProvider>
-      <AuthProvider>
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
-      </AuthProvider>
+        <AuthProvider>
+          <NavigationContainer>
+            <AppNavigator />
+          </NavigationContainer>
+        </AuthProvider>
     </SafeAreaProvider>
   );
 }
