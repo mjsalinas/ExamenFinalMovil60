f4ae26c07f60411b8b876a2f9ca21ebfb0397fe7
MarioTabora777 <46462339+MarioTabora777@users.noreply.github.com>
2026-03-23 21:03:57 -0600

    Supabase Auth — Autenticación
    -  se implementó login con signInWithPassword,validación de errores  y manejo de estado de usuario                                                                                      - Registro de email con signup.
    -  se implementó con signOut, limpia el estado
    -  al actualizar user, AppNavigator detecta el cambio y navega automáticamente a HomeTabs

--- MoneyLog/src/context/AuthContext.tsx
+++ MoneyLog/src/context/AuthContext.tsx
@@ -1,5 +1,8 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 import { User } from '../types';
+import { supabase } from '../services/supabaseClient'; 
+import { Alert } from "react-native";
+
 
 // ============================================================
 // AUTH CONTEXT
@@ -25,29 +28,81 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   const [isLoading, setIsLoading] = useState(false);
 
   // TODO (Inciso C.1): Reemplaza esta función con supabase.auth.signInWithPassword
-  const login = async (email: string, _password: string) => {
+  const login = async (email: string, password: string) => {
     setIsLoading(true);
     try {
-      // Implementación temporal — elimina esto al integrar Supabase
-      setUser({ id: 'mock-id', email });
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+      if (error) {
+        // throw new Error(error.message);  
+          console.log('Email' + email  + 'pass' + password)
+            Alert.alert("Error al iniciar sesion", error.message);
+
+
+      }
+      if (data.user) {
+        setUser({
+          id: data.user.id,
+          email: data.user.email ?? '',
+          name: data.user.user_metadata?.name,
+        });
+      }
     } finally {
       setIsLoading(false);
     }
-  };
+  }; 
+
+
+  //  const login = async (email: string, password: string) => {
+  //       const {data, error} = await supabase.auth.signInWithPassword({
+  //           email, 
+  //           password
+  //       });
+  //       if (error) {
+  //           Alert.alert("Error al iniciar sesion", error.message);
+  //       };
+  //      console.log("res data: ", data)
+  //   };
+  //   const logout = () =>{
+  //       setUser(null);
+  //       setIsAllowed(false);
+  //   };
+
 
   // TODO (Inciso C.2): Reemplaza esta función con supabase.auth.signUp
-  const register = async (name: string, email: string, _password: string) => {
+  const register = async (name: string, email: string, password: string) => {
     setIsLoading(true);
     try {
-      // Implementación temporal — elimina esto al integrar Supabase
-      setUser({ id: 'mock-id', email, name });
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+          data: { name },
+        },
+      });
+      if (error) {
+        // throw new Error(error.message); 
+            Alert.alert("Error al registrar"+ error.message);
+
+
+      }
+      if (data.user) {
+        setUser({
+          id: data.user.id,
+          email: data.user.email ?? '',
+          name,
+        });
+      }
     } finally {
       setIsLoading(false);
     }
   };
 
   // TODO (Inciso C.3): Implementa con supabase.auth.signOut
-  const logout = () => {
+  const logout = async () => {
+    await supabase.auth.signOut();
     setUser(null);
   };
 
